8f5a74b5bce5decbc09c7a40d9246e1dcc2094b3
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-06-03 17:40:41 +0300

    check bug in SimpleRecord for describe

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ $si = new AsceticCMS\Components\ServerInfo\ServerInfo();
  * @todo        normal main page
  */
 $app->get('', function () use ($si) {
-	$resp = new Response('200 Ok!', $si->show());
-	$resp->send();
+    $resp = new Response('200 Ok!', $si->show());
+    $resp->send();
 });
 
 /**
@@ -32,62 +32,62 @@ $app->get('', function () use ($si) {
  */
 $app->get('/api/table/:name/describe', function () {
 
-	$param = Request::cmpPlaceholder('/table/:name/describe', $_SERVER['REQUEST_URI']);
-	$tableName = $param['name'];
+    $param = Request::cmpPlaceholder('/api/table/:name/describe', $_SERVER['REQUEST_URI']);
+    $tableName = $param['name'];
 
-	$result = json_encode(SimpleRecord::describeTable($tableName), JSON_UNESCAPED_UNICODE);
-	$resp = new Response('200 JSON', $result);
-	$resp->send();
+    $result = json_encode(SimpleRecord::describeTable($tableName), JSON_UNESCAPED_UNICODE);
+    $resp = new Response('200 JSON', $result);
+    $resp->send();
 });
 
 $app->get('/api/table/:name', function () {
-	$param = array();
-	$param = Request::cmpPlaceholder('/api/table/:name', $_SERVER['REQUEST_URI']);
+    $param = array();
+    $param = Request::cmpPlaceholder('/api/table/:name', $_SERVER['REQUEST_URI']);
 
-	$tableName = $param['name'];
+    $tableName = $param['name'];
 
-	$result = json_encode(SimpleRecord::readTable($tableName, 10), JSON_UNESCAPED_UNICODE);
-	$resp = new Response('200 JSON', $result);
-	$resp->send();
+    $result = json_encode(SimpleRecord::readTable($tableName, 10), JSON_UNESCAPED_UNICODE);
+    $resp = new Response('200 JSON', $result);
+    $resp->send();
 });
 
 /**
  * calc zen of python. good word! (ruRU)
  */
 $app->get('/zen', function () use ($si) {
-	$text = array(
-		"zen" => array(
-			'🎖Красивое лучше, чем уродливое.',
-			'🎖Явное лучше, чем неявное.',
-			'🎖Простое лучше, чем сложное.',
-			'🎖Сложное лучше, чем запутанное.',
-			'🎖Плоское лучше, чем вложенное.',
-			'🎖Разреженное лучше, чем плотное.',
-			'🎖Читаемость имеет значение.',
-			'🎖Особые случаи не настолько особые, чтобы нарушать правила.',
-			'🎖При этом практичность важнее безупречности.',
-			'🎖Ошибки никогда не должны замалчиваться.',
-			'🎖Если не замалчиваются явно.',
-			'🎖Встретив двусмысленность, отбрось искушение угадать.',
-			'🎖Должен существовать один — и, желательно, только один — очевидный способ сделать это.',
-			'🎖Хотя он поначалу может быть и не очевиден, если вы не голландец[5].',
-			'🎖Сейчас лучше, чем никогда.',
-			'🎖Хотя никогда зачастую лучше, чем прямо сейчас.',
-			'🎖Если реализацию сложно объяснить — идея плоха.',
-			'🎖Если реализацию легко объяснить — идея, возможно, хороша.',
-			'🎖Пространства имён — отличная вещь! Давайте будем делать их больше!',
-		),
-	);
-	$resp = new Response('200 Ok!', json_encode($text, JSON_UNESCAPED_UNICODE));
-	$resp->send();
+    $text = array(
+        "zen" => array(
+            '🎖Красивое лучше, чем уродливое.',
+            '🎖Явное лучше, чем неявное.',
+            '🎖Простое лучше, чем сложное.',
+            '🎖Сложное лучше, чем запутанное.',
+            '🎖Плоское лучше, чем вложенное.',
+            '🎖Разреженное лучше, чем плотное.',
+            '🎖Читаемость имеет значение.',
+            '🎖Особые случаи не настолько особые, чтобы нарушать правила.',
+            '🎖При этом практичность важнее безупречности.',
+            '🎖Ошибки никогда не должны замалчиваться.',
+            '🎖Если не замалчиваются явно.',
+            '🎖Встретив двусмысленность, отбрось искушение угадать.',
+            '🎖Должен существовать один — и, желательно, только один — очевидный способ сделать это.',
+            '🎖Хотя он поначалу может быть и не очевиден, если вы не голландец[5].',
+            '🎖Сейчас лучше, чем никогда.',
+            '🎖Хотя никогда зачастую лучше, чем прямо сейчас.',
+            '🎖Если реализацию сложно объяснить — идея плоха.',
+            '🎖Если реализацию легко объяснить — идея, возможно, хороша.',
+            '🎖Пространства имён — отличная вещь! Давайте будем делать их больше!',
+        ),
+    );
+    $resp = new Response('200 Ok!', json_encode($text, JSON_UNESCAPED_UNICODE));
+    $resp->send();
 });
 
 /**
  * phpinfo()
  */
 $app->get('/phpinfo', function () {
-	$resp = new Response('200 Ok!', phpinfo());
-	$resp->send();
+    $resp = new Response('200 Ok!', phpinfo());
+    $resp->send();
 });
 
 /**
@@ -95,29 +95,29 @@ $app->get('/phpinfo', function () {
  * @todo        fix: delete
  */
 $app->get('/tst/1', function () {
-	$resp = new Response('200 Ok!', "HI! How are you?");
-	$resp->send();
+    $resp = new Response('200 Ok!', "HI! How are you?");
+    $resp->send();
 });
 
 $app->get('/tst/put', function () {
-	$tpl = new AsceticCMS\Components\Form\Form(['method' => 'PUT', 'action' => '/tst/user', 'name' => 'input', 'email' => 'input', 'submit' => 'send']);
-	$resp = new Response('200 Ok!', $tpl->show());
-	$resp->send();
+    $tpl = new AsceticCMS\Components\Form\Form(['method' => 'PUT', 'action' => '/tst/user', 'name' => 'input', 'email' => 'input', 'submit' => 'send']);
+    $resp = new Response('200 Ok!', $tpl->show());
+    $resp->send();
 });
 
 $app->get('/tst/db', function () {
-	$dt = new SimpleRecord();
-	$resp = new Response('200 Ok!', implode(";", $dt->readTable('users')));
-	//$resp = new Response('200 Ok!', phpinfo());
-	$resp->send();
+    $dt = new SimpleRecord();
+    $resp = new Response('200 Ok!', implode(";", $dt->readTable('users')));
+    //$resp = new Response('200 Ok!', phpinfo());
+    $resp->send();
 });
 
 $app->get('/tst/tpl', function () {
-	$ren = new Render();
-	$ren->run('./src/MVC/Views/View.tpl');
-	$s = implode('<br>', $ren->show());
-	$resp = new Response('200 Ok!', $s);
-	$resp->send();
+    $ren = new Render();
+    $ren->run('./src/MVC/Views/View.tpl');
+    $s = implode('<br>', $ren->show());
+    $resp = new Response('200 Ok!', $s);
+    $resp->send();
 });
 
 /**
@@ -125,12 +125,12 @@ $app->get('/tst/tpl', function () {
  * @todo        create standard form and delete test
  */
 $app->put('/tst/user', function () {
-	$resp = new Response('201 Ok!', "<strong>PUT response</strong><br><pre>" . $_POST['name'] . "<br>" . $_POST['email'] . "</pre>");
-	$resp->send();
+    $resp = new Response('201 Ok!', "<strong>PUT response</strong><br><pre>" . $_POST['name'] . "<br>" . $_POST['email'] . "</pre>");
+    $resp->send();
 });
 
 $app->get('/form', function () {
-	$conf = '{
+    $conf = '{
         "title": "User Card",
         "form": {
             "name": "testForm",
@@ -141,9 +141,9 @@ $app->get('/form', function () {
         "inputs": ["name", "surname", "Moscow", "Russia", "none", "+79259259259", "admin@mail.ru"],
         "submit": "Submit Query"
     }';
-	//FIXME!
-	$resp = new Response('200 Ok!', Render::renderView($conf, 'MailForm.phtml'));
-	$resp->send();
+    //FIXME!
+    $resp = new Response('200 Ok!', Render::renderView($conf, 'MailForm.phtml'));
+    $resp->send();
 });
 
 /**
