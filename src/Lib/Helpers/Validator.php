ebc1fe38c85b3a6ff0fd342d7ace15eb40f252db
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-06-06 17:28:35 +0300

    add chkWhitelist and Prepare methdos in Validator class, add readTableByParam method in SimpleRecord, add route in index.php

--- src/Lib/Helpers/Validator.php
+++ src/Lib/Helpers/Validator.php
@@ -1,7 +1,25 @@
 <?php
-namespace AsceticCMS\Validator;
-class Validator(){
-	public static functin tableNameValidate($param){
-		
+namespace AsceticCMS\Lib\Helpers;
+
+class Validator {
+	private static $whiteListSQL = array(
+		'NameOrg',
+	);
+	public static function tableNameValidate($param) {
+
+	}
+	public static function prepareString($param) {
+		if (!preg_match('/^[A-Za-z][A-Za-z0-9_]*$/', $param)) {
+			throw new AppSpecificSecurityException("Possible SQL injection attempt.");
+		} else {
+			return $param;
+		}
+	}
+	public static function chkWhiteList($param) {
+		if (in_array($param, self::$whiteListSQL)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
